02b6e238cac75cf9015937e94b0b8c5a69ccd792
Maria Magdalena Celińska <114350016+wyciszone@users.noreply.github.com>
2023-04-23 21:23:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,72 @@
 import random
 import nltk
+import random
+import nltk
 from nltk.corpus import gutenberg
 import PySimpleGUI as sg
 
 authors = {'Austen': 'austen-emma.txt', 'Bible': 'bible-kjv.txt', 'Blake':'blake-poems.txt', 'Bryant':'bryant-stories.txt', 'Burgess':'burgess-busterbrown.txt', 'Carroll':'carroll-alice.txt', 'Chesterton':'chesterton-ball.txt', 'Melville':'melville-moby_dick.txt', 'Milton':'milton-paradise.txt', 'Shakespeare':'shakespeare-macbeth.txt'}
 
-
 class Markov(object):
-	
-	def __init__(self, open_file):
-		self.cache = {}
-		self.open_file = open_file
-		self.words = self.file_to_words()
-		self.word_size = len(self.words)
-		self.database()
-		
-	
-	def file_to_words(self):
-		self.open_file.seek(0)
-		data = self.open_file.read()
-		words = data.split()
-		return words
-		
-	
-	def triples(self):
-		if len(self.words) < 3:
-			return
-		
-		for i in range(len(self.words) - 2):
-			yield (self.words[i], self.words[i+1], self.words[i+2])
-			
-	def database(self):
-		for w1, w2, w3 in self.triples():
-			key = (w1, w2)
-			if key in self.cache:
-				self.cache[key].append(w3)
-			else:
-				self.cache[key] = [w3]
+    
+    def __init__(self, open_file):
+        self.cache = {}
+        self.open_file = open_file
+        self.words = self.file_to_words()
+        self.word_size = len(self.words)
+        self.database()
+        
+    
+    def file_to_words(self):
+        self.open_file.seek(0)
+        data = self.open_file.read()
+        words = data.split()
+        return words
+        
+    
+    def triples(self):
+        if len(self.words) < 3:
+            return
+        for i in range(len(self.words) - 2):
+            yield (self.words[i], self.words[i+1], self.words[i+2])
+            
+    def database(self):
+        for w1, w2, w3 in self.triples():
+            key = (w1, w2)
+            if key in self.cache:
+                self.cache[key].append(w3)
+            else:
+                self.cache[key] = [w3]
 
-	def generate_markov_text(self, size=random.randint(60,180), seed_words=None):
-		if seed_words is None:
-			seed = random.randint(0, self.word_size-3)
-			seed_words = [self.words[seed], self.words[seed+1]]
-		else:
-			seed_words = seed_words.split()
-		w1, w2 = seed_words[0], seed_words[1]
-		gen_words = []
-		for i in range(size):
-			gen_words.append(w1)
-			w1, w2 = w2, random.choice(self.cache[(w1, w2)])
-		gen_words.append(w2)
-		return ' '.join(gen_words)
+    def generate_markov_text(self, size=random.randint(60,180), seed_words=None):
+        if seed_words is None:
+            seed = random.randint(0, self.word_size-3)
+            seed_words = [self.words[seed], self.words[seed+1], self.words[seed+2]]
+        else:
+            seed_words = seed_words.split()[-3:]
+        gen_words = []
+        w1, w2, w3 = seed_words
+        for i in range(size):
+            gen_words.append(w1)
+            try:
+                w1, w2, w3 = w2, w3, random.choice(self.cache[(w1, w2)])
+            except KeyError:
+                w1, w2, w3 = self.words[random.randint(0, self.word_size-3)], self.words[random.randint(0, self.word_size-2)], self.words[random.randint(0, self.word_size-1)]
+        gen_words.extend([w2, w3])
+        return ' '.join(gen_words)
 
+# window layout
 
 sg.theme('DarkAmber')
 
 layout = [  [sg.Text("Please choose an author:")],
             [sg.Combo(list(authors.keys()), key='-AUTHORS-')],
-	    	[sg.Text('Input starting words:')],
-	    	[sg.Input(key='-SEED-')],
+            [sg.Text('Input starting words:')],
+            [sg.Input(key='-SEED-')],
             [sg.Button('Generate'), sg.Button('Cancel')] ]
 
-# Create the Window
+# create the Window
+
 window = sg.Window('Text generator', layout)
 
 while True:
